df2f7d1d87f537eef32a155fa2c4ba7abdfeaa57
Mark Wong <markwong.digital@gmail.com>
2022-02-04 14:49:13 -0800

    Initial Commit

---
+++ Accompany/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Accompany
+//
+//  Created by Mark Wong on 2022-02-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+

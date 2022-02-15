b154df98facaafb7a66d08ff48fe117a59c4cee1
Mark Wong <markwong.digital@gmail.com>
2022-02-15 14:58:15 -0800

    Add Contraction timer tab

---
+++ Accompany/Controllers/Core/Timer/ContractionTimerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ContractionTimerViewController.swift
+//  Accompany
+//
+//  Created by Mark Wong on 2022-02-15.
+//
+
+import UIKit
+
+class ContractionTimerViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+    
+}

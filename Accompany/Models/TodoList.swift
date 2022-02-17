57d8ea1d63c4874fd31ba310e7f60855c66c88f1
Mark Wong <markwong.digital@gmail.com>
2022-02-16 17:32:22 -0800

    Add TodoList model

---
+++ Accompany/Models/TodoList.swift
@@ -0,0 +1,25 @@
+//
+//  TodoList.swift
+//  Accompany
+//
+//  Created by Mark Wong on 2022-02-16.
+//
+
+import Foundation
+
+enum Trimester: String {
+  
+  case firstTrimester = "1st Trimester"
+  case secondTrimester = "2nd Trimester"
+  case thirdTrimester = "3rd Trimester"
+  case after = "After"
+  
+}
+
+struct TodoList {
+  
+  var id = UUID()
+  var trimester: Trimester
+  var todos: [Todo]?
+  
+}
--- Accompany/Views/TodoList/TodoListTitleLabel.swift
+++ Accompany/Views/TodoList/TodoListTitleLabel.swift
@@ -9,6 +9,7 @@ import UIKit
 import SnapKit
 
 class TodoListTitleLabel: UILabel {
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
   }
@@ -23,4 +24,5 @@ class TodoListTitleLabel: UILabel {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
+  
 }

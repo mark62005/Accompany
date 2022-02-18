3a24a0f608c5d76a4df1bd4e519c78e6ffca5dbd
esperanzacc <v.esperanzac113@gmail.com>
2022-02-17 16:06:13 -0800

    Edit TitleLabel and SecondaryButton

--- Accompany/Views/TitleLabel.swift
+++ Accompany/Views/TitleLabel.swift
@@ -8,34 +8,50 @@
 import UIKit
 
 class TitleLabel: UILabel {
+  
+  enum Size {
+    case mini, small, medium, large
+  }
+  
+  enum Color {
+    case red, purple, black
+  }
 
   override init(frame: CGRect) {
     super.init(frame: .zero)
   }
-
-  convenience init() {
-    self.init(frame: .zero)
-    self.translatesAutoresizingMaskIntoConstraints = false
-    self.font = UIFont(name: "SimpleBoy", size: 60)
-    self.textColor = #colorLiteral(red: 0.9921568627, green: 0.3882352941, blue: 0.5333333333, alpha: 1)
-  }
   
-  convenience init(title: String) {
+  convenience init(title: String?, size: Size, color: Color = .red) {
     self.init(frame: .zero)
-    self.translatesAutoresizingMaskIntoConstraints = false
-    self.text = title
-    self.textAlignment = .center
-    self.font = UIFont(name: "SimpleBoy", size: 45)
     self.textColor = #colorLiteral(red: 0.9921568627, green: 0.3882352941, blue: 0.5333333333, alpha: 1)
-  }
+    self.textAlignment = .center
+    self.numberOfLines = 0
+    self.translatesAutoresizingMaskIntoConstraints = false
+    
+    if let title = title {
+      self.text = title
+    }
+     
+    switch size {
+    case .mini:
+      self.font = UIFont.boldSystemFont(ofSize: 18)
+    case .small:
+      self.font = UIFont(name: "SimpleBoy", size: 25)
+    case .medium:
+      self.font = UIFont(name: "SimpleBoy", size: 45)
+    case .large:
+      self.font = UIFont(name: "SimpleBoy", size: 60)
+    }
+    
+    switch color {
+    case .red:
+      self.textColor = #colorLiteral(red: 0.9921568627, green: 0.3882352941, blue: 0.5333333333, alpha: 1)
+    case .purple:
+      self.textColor = #colorLiteral(red: 0.5490196078, green: 0.4588235294, blue: 0.968627451, alpha: 1)
+    case .black:
+      self.textColor = #colorLiteral(red: 0.2078431373, green: 0.2039215686, blue: 0.2235294118, alpha: 1)
+    }
   
-  convenience init(smallerTitle: String) {
-    self.init(frame: .zero)
-    self.translatesAutoresizingMaskIntoConstraints = false
-    self.text = smallerTitle
-    self.textAlignment = .center
-    self.font = .boldSystemFont(ofSize: 25)
-    self.textColor = #colorLiteral(red: 0.9921568627, green: 0.3882352941, blue: 0.5333333333, alpha: 1)
   }
   
   required init?(coder: NSCoder) {
